f4d7d86d564cf284827694b25db6984b3713d349
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-11-05 17:20:20 -0500

    First answer done Connected

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+//instantiate program object
+$obj=new main();
 
-
+  class main
+  {
+    public function __construct()
+    {
+    //using PDO to connect to MySQL Database	
+    $dsn = 'mysql:dbname=mr669;host=sql1.njit.edu';
+    $user = 'mr669';
+    $password = 'abHYFGPw';
+    try {
+        global $dbconn;
+	    $dbconn = new PDO($dsn, $user, $password);
+	    $dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    //1. If connected print
+	    echo 'Connected successfully <br>';
+        } 
+    catch (PDOException $e) {
+    //1. If not connected print	
+	echo 'Connection failed: ' . $e->getMessage() . '<br>';
+      }
+     }
+   }
 ?>
