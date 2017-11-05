2a7d2e3538059d87d7cf71b6261bde500ddf943e
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-11-05 17:44:39 -0500

    indent

--- index.php
+++ index.php
@@ -21,10 +21,11 @@ $obj=new main();
 	    echo 'Connected successfully <br>';
 	    $this::testConn($dbconn);
         } 
-    catch (PDOException $e) {
-    //1. If not connected print	
+    catch (PDOException $e)
+        {
+        //1. If not connected print	
 	echo 'Connection failed: ' . $e->getMessage() . '<br>';
-      }
+        }
      }
      //function to perform selection and display
      public static function testConn($dbconn) 
@@ -43,22 +44,22 @@ $obj=new main();
          }
      //3.Count records and Display table
  	 if(count($result) > 0)
-     {
-     echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
+         {
+          echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Pass</th></tr>";
      $i=0;
-     foreach ($result as $label) 
+           foreach ($result as $label) 
             {
      		if($label["id"]<6){
                        $i=$i+1;
-                       //Display
-                       echo"<tr><td>".$label["id"]."</td><td>".$label["email"]."</td><td>".$label["fname"]."</td><td>".$label["lname"]."</td><td>".$label["phone"]."</td><td>".$label["birthday"]."</td><td>".$label["gender"]."</td><td>".$label["password"]."</td></tr>";
+                 //Display
+                echo"<tr><td>".$label["id"]."</td><td>".$label["email"]."</td><td>".$label["fname"]."</td><td>".$label["lname"]."</td><td>".$label["phone"]."</td><td>".$label["birthday"]."</td><td>".$label["gender"]."</td><td>".$label["password"]."</td></tr>";
                               }
             }
     //3.Count records        
     echo "The number of records in the result is: $i";
     echo '</br>';
     
-     }
+         }
      else{
      echo "0 results";
          }
